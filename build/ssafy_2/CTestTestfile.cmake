a1c487d7e137e5254dd9990bf5c6f2f5fd257172
sehoon <tpehd1009@gmail.com>
2023-09-06 15:13:19 +0900

    gggg

--- build/ssafy_2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kshssafy/catkin_ws/src/ssafy_2
-# Build directory: /home/kshssafy/catkin_ws/build/ssafy_2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

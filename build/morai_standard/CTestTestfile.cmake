fdde0c15db847b4d6b59ffd8daf6ee2e70a7581b
박근창 <goodi421@naver.com>
2023-09-06 13:52:15 +0900

    test

---
+++ build/morai_standard/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kshssafy/catkin_ws/src/morai_standard
+# Build directory: /home/kshssafy/catkin_ws/build/morai_standard
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ devel/share/gennodejs/ros/morai_msgs/srv/_index.js
@@ -0,0 +1,34 @@
+
+"use strict";
+
+let MoraiScenarioLoadSrv = require('./MoraiScenarioLoadSrv.js')
+let MoraiSyncModeSetGearSrv = require('./MoraiSyncModeSetGearSrv.js')
+let MoraiWaitForTickSrv = require('./MoraiWaitForTickSrv.js')
+let MoraiSyncModeSLSrv = require('./MoraiSyncModeSLSrv.js')
+let MoraiSyncModeCmdSrv = require('./MoraiSyncModeCmdSrv.js')
+let MoraiSyncModeRemoveObjectSrv = require('./MoraiSyncModeRemoveObjectSrv.js')
+let MoraiVehicleSpecSrv = require('./MoraiVehicleSpecSrv.js')
+let MoraiSimProcSrv = require('./MoraiSimProcSrv.js')
+let MoraiSyncModeAddObjectSrv = require('./MoraiSyncModeAddObjectSrv.js')
+let MoraiTLInfoSrv = require('./MoraiTLInfoSrv.js')
+let MoraiMapSpecSrv = require('./MoraiMapSpecSrv.js')
+let MoraiSyncModeCtrlCmdSrv = require('./MoraiSyncModeCtrlCmdSrv.js')
+let MoraiEventCmdSrv = require('./MoraiEventCmdSrv.js')
+let PREventSrv = require('./PREventSrv.js')
+
+module.exports = {
+  MoraiScenarioLoadSrv: MoraiScenarioLoadSrv,
+  MoraiSyncModeSetGearSrv: MoraiSyncModeSetGearSrv,
+  MoraiWaitForTickSrv: MoraiWaitForTickSrv,
+  MoraiSyncModeSLSrv: MoraiSyncModeSLSrv,
+  MoraiSyncModeCmdSrv: MoraiSyncModeCmdSrv,
+  MoraiSyncModeRemoveObjectSrv: MoraiSyncModeRemoveObjectSrv,
+  MoraiVehicleSpecSrv: MoraiVehicleSpecSrv,
+  MoraiSimProcSrv: MoraiSimProcSrv,
+  MoraiSyncModeAddObjectSrv: MoraiSyncModeAddObjectSrv,
+  MoraiTLInfoSrv: MoraiTLInfoSrv,
+  MoraiMapSpecSrv: MoraiMapSpecSrv,
+  MoraiSyncModeCtrlCmdSrv: MoraiSyncModeCtrlCmdSrv,
+  MoraiEventCmdSrv: MoraiEventCmdSrv,
+  PREventSrv: PREventSrv,
+};

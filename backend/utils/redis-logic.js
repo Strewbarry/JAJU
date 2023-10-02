f9c1b61318ab6bd784bf463d35a1d7574e7fd4ed
김민국 <eoals6511@naver.com>
2023-10-02 19:43:24 +0900

    [BE]<FIX> region 기본값 빼고 프론트 설정값으로 넣기

--- backend/utils/redis-logic.js
+++ backend/utils/redis-logic.js
@@ -28,8 +28,31 @@ const redis = new Redis({
     redis.quit(); // 연결 종료
   }
 }
-const a = 'asdf'
-const b = 'as'
+// const a = 'asdf'
+// const b = 'as'
 // setValue(a, 1);
+async function checkRedisValue() {
+  const Redis = require('ioredis');
 
-module.exports = {setValue}
+// Redis 서버에 연결
+const redis = new Redis({
+  host: 'j9c104.p.ssafy.io',
+  port: 6379, // Redis 기본 포트
+  password: 'c104' // 설정된 경우에만 필요
+});
+  try {
+    const value = await redis.get('status'); // 여기에 조회하고 싶은 Redis 키를 입력하세요
+    if (value !== null) {
+      console.log('Redis 값:', value);
+      
+    } else {
+      console.log('Redis에 저장된 값이 없습니다.');
+    }
+  } catch (error) {
+    console.error('Redis 조회 오류:', error);
+  }
+}
+// setInterval(checkRedisValue, 1000)
+
+
+module.exports = {setValue, checkRedisValue}

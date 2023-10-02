6b6879f0f3397fe27ab46b9efc3d07c1c4e06fe4
김민국 <eoals6511@naver.com>
2023-10-02 19:49:40 +0900

    [BE]<fiX> 바로 예약할 때는 region 생략

--- backend/logics/reservation-logic.js
+++ backend/logics/reservation-logic.js
@@ -46,8 +46,8 @@ exports.now = async (req, res) => {
     });
     const vehicle_id = req.body.vehicle_id
     const reservation_time = new Date()
-    const sql = 'INSERT INTO reservation (`user_id`, `vehicle_id`, `reservation_time`,`return_time`,`region`, `lat`, `lng`) VALUES (?,?,?,?,?,?,?)'
-    const params = [user_id[0].id, vehicle_id, reservation_time,reservation_time, req.body.region, req.body.lat, req.body.lng]
+    const sql = 'INSERT INTO reservation (`user_id`, `vehicle_id`, `reservation_time`,`return_time`, `lat`, `lng`) VALUES (?,?,?,?,?,?)'
+    const params = [user_id[0].id, vehicle_id, reservation_time,reservation_time, req.body.lat, req.body.lng]
 
     connection.query(sql, params, function (error, results, fields) {
         if (error) throw error;

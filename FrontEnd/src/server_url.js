6059459bf71eece5a31a90eb1a586832c82e18d3
이성연 <chamsy39@gmail.com>
2023-10-04 15:41:54 +0900

    [FE]<DESIGN> 회원가입 수정

--- FrontEnd/src/server_url.js
+++ FrontEnd/src/server_url.js
@@ -1,12 +1,8 @@
-
-
 // export const Url = 'http://localhost:3001'
 
-
 // 민국 백서버
-// export const Url = 'http://192.168.100.38:3001'
+export const Url = "http://192.168.100.38:3000";
 
-
-export const url = 'http://j9c104.p.ssafy.io:3001/'
+// export const url = 'http://j9c104.p.ssafy.io:3001/'
 
 // module.exports = url

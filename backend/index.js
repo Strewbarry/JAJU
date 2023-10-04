8c880b10a81d3eaf1d011f7310d6d1bf914b985a
김민국 <eoals6511@naver.com>
2023-10-04 09:29:03 +0900

    [BE] sprint start

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,24 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'test',
-  password : 'test',
-  database : 'test'
-});
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'test',
+//   password : 'test',
+//   database : 'test'
+// });
 
 
-exports.read_users = () => {
-    connection.connect();
+// exports.read_users = () => {
+//     connection.connect();
  
-    connection.query('SELECT * from USER', function (error, results, fields) {
-    if (error) throw error;
-    console.log('users: ', results);
+//     connection.query('SELECT * from USER', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('users: ', results);
     
-    res = results
-    });
+//     res = results
+//     });
     
-    connection.end();
-    return res
-      .status(200)
-      .json({ code: 200, message: "요청 처리 성공", data: result });
-}
+//     connection.end();
+//     return res
+//       .status(200)
+//       .json({ code: 200, message: "요청 처리 성공", data: result });
+// }
--- backend/utils/redis-logic.js
+++ backend/utils/redis-logic.js
@@ -2,25 +2,32 @@
 
 // Redis에 값을 저장
 async function setValue (a, select) {
-    const Redis = require('ioredis');
+  const Redis = require('ioredis');
 
-// Redis 서버에 연결
-const redis = new Redis({
-  host: 'j9c104.p.ssafy.io',
-  port: 6379, // Redis 기본 포트
-  password: 'c104' // 설정된 경우에만 필요
-});
+  // Redis 서버에 연결
+  const redis = new Redis({
+    host: 'j9c104.p.ssafy.io',
+    port: 6379, // Redis 기본 포트
+    password: 'c104' // 비밀번호
+  });
   try {
+    // select가 1이라면 목적지로 출발하라는 신호이니
     if ( select === 1) {
-
-        // const x = a.toString()
-        // const y = b.toString()
+      // redis status 값을 start로 변경
         await redis.set('status', 'start')
+      // cmd 값을 목적지 id값으로 변경
         await redis.set('cmd', a);
         console.log('값이 설정되었습니다.');
     }
-    else {
+    // 0이라면 정지 신호이니
+    else if (select === 0) {
+      // redis status 를 stop으로 변경
         await redis.set('status', 'stop')
+    }
+    // 이후 나머지는 호출지로 호출하는 신호이다.
+    else {
+      // redis status 를 call로 변경
+      await redis.set('status', 'call')
     }
   } catch (error) {
     console.error('오류 발생:', error);
@@ -28,23 +35,23 @@ const redis = new Redis({
     redis.quit(); // 연결 종료
   }
 }
-// const a = 'asdf'
-// const b = 'as'
-// setValue(a, 1);
+
 async function checkRedisValue() {
   const Redis = require('ioredis');
 
-// Redis 서버에 연결
-const redis = new Redis({
-  host: 'j9c104.p.ssafy.io',
-  port: 6379, // Redis 기본 포트
-  password: 'c104' // 설정된 경우에만 필요
-});
+  // Redis 서버에 연결
+  const redis = new Redis({
+    host: 'j9c104.p.ssafy.io',
+    port: 6379, // Redis 기본 포트
+    password: 'c104' // 설정된 경우에만 필요
+  });
   try {
-    const value = await redis.get('status'); // 여기에 조회하고 싶은 Redis 키를 입력하세요
+    // 찾고 싶은 redis 값
+    const value = await redis.get('status'); 
+    // 그 값이 있다면
     if (value !== null) {
       console.log('Redis 값:', value);
-      
+    // 그 값이 없다면
     } else {
       console.log('Redis에 저장된 값이 없습니다.');
     }
@@ -52,7 +59,7 @@ const redis = new Redis({
     console.error('Redis 조회 오류:', error);
   }
 }
-// setInterval(checkRedisValue, 1000)
+
 
 
 module.exports = {setValue, checkRedisValue}

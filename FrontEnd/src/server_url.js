17a3015bbaa46ca05d0250e288504e08d2fa89cc
임채상 <limcs1998@naver.com>
2023-10-04 15:08:18 +0900

    배포 url로 변경

--- FrontEnd/src/server_url.js
+++ FrontEnd/src/server_url.js
@@ -4,9 +4,9 @@
 
 
 // 민국 백서버
-export const Url = 'http://192.168.100.38:3001'
+// export const Url = 'http://192.168.100.38:3001'
 
 
-// export const url = 'http://j9c104.p.ssafy.io:3000/'
+export const url = 'http://j9c104.p.ssafy.io:3001/'
 
 // module.exports = url
